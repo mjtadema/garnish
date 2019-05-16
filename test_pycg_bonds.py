a9abfa9a0531396d7f7db01cfb2a3a90994edcc4
Matthijs Tadema <M.J.Tadema@gmail.com>
2019-05-16 17:14:24 +0200

    Removed get_chains test, function doesn't exist anymore

--- test_pycg_bonds.py
+++ test_pycg_bonds.py
@@ -3,16 +3,6 @@ from pymol import cmd, stored, CmdException
 import os
 from pdb import set_trace as bp
 
-def test_get_chains():
-    cmd.reinitialize()
-    for selection in ["cg_monomer","cg_oligomer"]:
-        cmd.load(selection+".pdb")
-        chains = target.get_chains(selection)
-        if len(chains) < 1:
-            raise ValueError("got no chains")
-        elif selection == "cg_oligomer" and len(chains) <= 1:
-            raise ValueError("not enough chains for oligomer")
-
 def test_get_chain_bb():
     cmd.reinitialize()
     selection = "cg_oligomer_nochains"
@@ -22,7 +12,6 @@ def test_get_chain_bb():
     target.get_chain_bb(selection, chains)
 
 test_list = [
-        test_get_chains,
         test_get_chain_bb
         ]
 
