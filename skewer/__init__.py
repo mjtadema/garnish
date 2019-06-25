ebe5913e49bd4c967b32c5e5fd588f33b55ce35f
Lorenzo Gaifas <brisvag@gmail.com>
2019-06-25 15:07:01 +0200

    parent module check now catches exceptions

--- skewer/__init__.py
+++ skewer/__init__.py
@@ -2,11 +2,18 @@ from skewer.utils import clean_path
 import sys
 import os
 
-# check if module was called by pymol, if yes run skewer.main
-main_initfile = sys.modules['__main__'].__file__
 
-# get the name of the parent module
-main_modulename = os.path.basename(clean_path(os.path.join(main_initfile, os.pardir)))
+try:
+    # check if module was called by pymol, if yes run skewer.main
+    main_initfile = sys.modules['__main__'].__file__
+
+    # get the name of the parent module
+    main_modulename = os.path.basename(clean_path(os.path.join(main_initfile, os.pardir)))
+
+except AttributeError:
+    # importing from an interpreter like ipython raises this error
+    main_modulename = None
+
 
 if main_modulename == 'pymol':
     from skewer.skewer import load
