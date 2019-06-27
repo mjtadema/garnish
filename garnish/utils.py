6d543a79373dc0badfb0b19918e4e713456a60ba
Lorenzo Gaifas <brisvag@gmail.com>
2019-06-27 13:44:49 +0200

    fixed impossible extension outside of pymol

--- garnish/utils.py
+++ garnish/utils.py
@@ -84,7 +84,7 @@ def extension(loading_func):
     try:
         # check if module was called by pymol
         main_initfile = sys.modules['__main__'].__file__
-    
+
         # get the name of the parent module
         main_modulename = os.path.basename(clean_path(os.path.join(main_initfile, os.pardir)))
     
