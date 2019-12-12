02eda767ae5ba967defc2438196dddd1cba80e87
Lorenzo Gaifas <brisvag@gmail.com>
2019-12-12 12:33:37 +0100

    fixed boolean usage in garnish

--- garnish/garnish.py
+++ garnish/garnish.py
@@ -9,7 +9,7 @@ from .system import System
 from .utils import get_chain_bb
 
 
-def garnish(file=None, selection='all', gmx=None, fix_elastics=True, guess_bb=True, show=True):
+def garnish(file=None, selection='all', gmx=None, fix_elastics=1, guess_bb=1, show=1):
     """
 DESCRIPTION
 
@@ -30,9 +30,15 @@ ARGUMENTS
     file = a tpr or topology file to extract bond information from (default: None)
     selection = any selection to act upon (default: all)
     gmx = gmx executable path (default: inferred by `which gmx`)
-    elastic_fix = fix elastic bonds based on atom id.
-                  Disable if your beads are numbered non-sequentially (default: True)
+    fix_elastics = fix elastic bonds based on atom id. Disable if your beads are
+                   numbered non-sequentially (default: 1)
+    guess_bb = if file is not present, simply draw bonds between backbone atoms (default: 1)
+    show = adjust representation after drawing bonds (default: 1)
     """
+    fix_elastics = bool(int(fix_elastics))
+    guess_bb = bool(int(guess_bb))
+    show = bool(int(show))
+
     # Retain order so pymol does not sort the atoms, giving a different result when saving the file
     cmd.set("retain_order", 1)
 
@@ -40,8 +46,6 @@ ARGUMENTS
         # parse the file
         sys_dict = parse(file, gmx)
         # create System object and draw all the bonds
-        if fix_elastics in ('False', '0'):  # TODO: hack
-            fix_elastics = False
         system = System(sys_dict, fix_elastics=fix_elastics)
         system.draw_bonds(selection)
         system.transfer_attributes(selection)
