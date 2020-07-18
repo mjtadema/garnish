174a4179be56d38963e1361d7128a07f3440a848
Matthijs Tadema <M.J.Tadema@protonmail.com>
2020-07-18 17:51:17 +0200

    Changed the import mechanism for pymol extensions

--- garnish/__init__.py
+++ garnish/__init__.py
@@ -1,6 +1,17 @@
 # Copyright 2019-2019 the garnish authors. See copying.md for legal info.
+from pathlib import Path
+import sys
 
-from .utils import extension
+from .parser import Parser
 
-if __name__ == "pymol":
+# Check if importing from pymol
+try:
+    # check if module was called by pymol
+    main_module_name = Path(sys.modules['__main__'].__file__).parent.name
+except AttributeError:
+    # importing from an interpreter like ipython raises this error
+    main_module_name = None
+
+print(main_module_name)
+if main_module_name == 'pymol':
     from . import extensions
